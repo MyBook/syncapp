114c75307b25b6237d6531b3656b4578a2c29ca5
Fedor Nasyrov <name.zh@gmail.com>
2017-06-27 15:41:07 +0300

    added media root

--- syncapp/urls.py
+++ syncapp/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from syncapp import views
@@ -21,3 +24,6 @@ urlpatterns = [
     url(r'', views.ResultView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
